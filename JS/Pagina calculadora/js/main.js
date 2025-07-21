ecd9288123b0ce34295a3606d587f6994dcac38d
JuanOsorner <juanpowerlifting1000kg@gmail.com>
2025-07-21 11:09:04 -0500

    Añadiendo-Carpetas-NuevasTec-FrontEdn2

--- JS/Pagina calculadora/js/main.js
+++
@@ -1,6 +0,0 @@
-import { st } from './st.js';
-import { Usuario } from './Usuario.js';
-const cerrarModal = document.getElementById('cerrarModal');
-cerrarModal.addEventListener('click',()=>{
-    document.getElementById('Modal').style.display = none;
-});
