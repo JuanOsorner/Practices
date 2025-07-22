1f8efbdeb42289fd5d1f064acda973801521699e
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-07-21 20:48:53 -0500

    Clase#3

---
+++ A-BackEnd2/Clase3/classRoomaAPPv3/src/main/java/com/example/classRoomaAPPv3/ClassRoomaApPv3Application.java
@@ -0,0 +1,13 @@
+package com.example.classRoomaAPPv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassRoomaApPv3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassRoomaApPv3Application.class, args);
+	}
+
+}
