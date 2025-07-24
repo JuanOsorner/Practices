955c0cae5bee701c2a723887977e5ef278327bce
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-07-23 20:16:20 -0500

    CLASE3

--- Clase#3/promesas.js
+++ Clase#3/promesas.js
@@ -28,17 +28,39 @@ let peliculas = [
         "portada": ""
     }
 ]
+// Investigar de que trata esto:
 function obtenerPeliculas(pelicula){
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             if(pelicula.length > 0){
-                resolve(pelis);
+                resolve(pelicula);
             }else{
                 reject("Error, No hay datos en la BD")
             }
         },2000); 
     });
 }
-const btn
-//Mostrar la información 
-console.table(obtenerPeliculas(peliculas));
+//forma 1 de resolver la promesa: 
+const btn = document.getElementById('boton1')
+/*btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    //Then es para cuando entrega la informacion
+    obtenerPeliculas(peliculas).then((p)=>{
+        console.table(p)
+    }).catch((error)=>{
+        alert("EL QUE LEE ES GAY")
+    })
+});*/
+//forma 2: resolver una promesa 
+async function  mostrarPeliculas(movies){
+    try{
+        let respuesta = await obtenerPeliculas(movies);
+        console.table(respuesta)
+    }catch(error){
+        alert(error);
+    }
+}
+btn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    mostrarPeliculas(peliculas);
+});
