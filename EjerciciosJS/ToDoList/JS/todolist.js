b4118d1f9891fab4c81464245266c5a9b616b3fd
JuanOsorner <juanpowerlifting1000kg@gmail.com>
2025-07-25 17:18:46 -0500

    AvanzandoPracticav2

--- EjerciciosJS/ToDoList/JS/todolist.js
+++ EjerciciosJS/ToDoList/JS/todolist.js
@@ -1,6 +1,7 @@
+import { localStorageTodoList } from "./localStorageTodoList.js";
 const newUl = document.getElementById('taskList');
-const buttonAddtask = document.getElementById('taskAddButton')
-buttonAddtask.addEventListener('click',(e)=>{
+const taskAddbutton = document.getElementById('taskAddButton');
+taskAddbutton.addEventListener('click',(e)=>{
     e.preventDefault();
     const taskInput = document.getElementById('taskInput').value;
     //(1)Creamos un li
@@ -19,21 +20,34 @@ buttonAddtask.addEventListener('click',(e)=>{
         eve.preventDefault();
         newLi.classList.add("completed");
         newElementCompletedbutton.textContent = "✅";
+        const tarea = {
+            nombreTarea: taskInput,
+            completada: "si"
+        }
     });
     newElementdiv.appendChild(newElementCompletedbutton);
     //(4)Creamos el boton basura
     const newElementDeletebutton = document.createElement('button');
     newElementDeletebutton.classList.add("delete-btn")
     newElementDeletebutton.textContent = "🗑️";
+    newElementDeletebutton.addEventListener('click',(even)=>{
+        even.preventDefault();
+        localStorageTodoList.eliminarDato(taskInput);
+        //localStorageTodoList.eliminarTodosLosDatos();
+    })
     newElementdiv.appendChild(newElementDeletebutton);
     //(5)Creamos el metodo eliminar tarea
     newElementDeletebutton.addEventListener('click',(ev)=>{
         ev.preventDefault();
         newUl.removeChild(newLi);
     });
-    
+    //------------------------------------------------------------
+    const tarea = {
+        nombreTarea: taskInput,
+        completada: "no"
+    };
+    localStorageTodoList.guardarDatos(tarea);
 });
-
 //Con appendchild marcamos la gerarquia de la creación 
 //Podemos usar removechild para eliminar 
 // Es mejor usar .classlist.add para añadir la clase
