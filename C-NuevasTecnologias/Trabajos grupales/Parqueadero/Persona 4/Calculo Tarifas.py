6e2e5147070814e2c736e3b5a1b92699ea28394e
JuanOsorner <juanpowerlifting1000kg@gmail.com>
2025-08-04 20:50:24 -0500

    feat subiendoFuncionCalcular

--- C-NuevasTecnologias/Trabajos grupales/Parqueadero/Persona 4/Calculo Tarifas.py
+++ C-NuevasTecnologias/Trabajos grupales/Parqueadero/Persona 4/Calculo Tarifas.py
@@ -1,13 +1,26 @@
 def calcularTarifas(horas,tipo):
+    #Tipo == '0' va a ser para los vehiculos
     if tipo == '0':
-        if horas < 1:
-            return 0
-        elif horas < 5:
-            return 4000*horas
-        elif horas < 24:
-            return 20000
+        tarifa_hora = 4000
+        tarifa_dia = 20000
+    #Tipo == '1' va a ser para las motos
+    elif tipo == '1':
+        tarifa_hora = 2000
+        tarifa_dia = 10000
+    else:
+        return f"Valor {tipo} invalido"
+    if horas < 1:
+        return 0
+    elif horas < 5:
+        return int(tarifa_dia * horas)
+    else:
+        diaCompleto = int(horas//24)
+        horasRestantes = int(horas % 24)
+        if diaCompleto < 1:
+            return diaCompleto*tarifa_dia
+        elif horasRestantes < 5:
+            return diaCompleto*tarifa_dia + horasRestantes*tarifa_hora
         else:
-            valor = 20000
-            if int(horas) % 24 != 0:
-               valor = valor + 4000*(int(horas) % 24)
-            return valor
+            return (diaCompleto+1)*tarifa_dia
+
+print(calcularTarifas(36,'0'))
